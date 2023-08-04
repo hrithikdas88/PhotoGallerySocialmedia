9e49a2ceb158c1935ecda436c444c4f365bd2502
Hrithik_wac_react <hrithik.d@webandcrafts.com>
2023-08-04 14:31:01 +0530

    started homepage design

--- client/src/pages/homePage/HomePage.jsx
+++ client/src/pages/homePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { setLogout } from "store/authSlice";
 import { useNavigate } from "react-router-dom";
+import './HomePage.scss'
 
 const HomePage = () => {
   const dispatch = useDispatch();
@@ -11,8 +12,29 @@ const HomePage = () => {
     navigate("/");
   };
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="main-div">
+      <div className="profile-message">
+        <div className="profile">
+          <h1>profile</h1>
+        </div>
+        <div className="message">
+          <h1>message</h1>
+        </div>
+      </div>
+      <div className="addpost-feed">
+        <div className="addpost">
+          <h1>addpost</h1>
+        </div>
+        <div className="feed">
+          <h1>feed</h1>
+        </div>
+      </div>
+      <div className="friend-list">
+        <div className="freindlist">
+          <h1>friendlist</h1>
+        </div>
+      </div>
+     
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
